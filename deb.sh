c18fff45cb0cda4566e8b13f85a4d511799836f4
TsikyAndriatahiana <antsah.andriatahina@gmail.com>
2025-11-28 20:51:04 +0100

    Update deb.sh to enable CGI module

--- deb.sh
+++ deb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo mkdir -p /var/www/html/debs
 sudo cp /var/cache/apt/archives/*.deb /var/www/html/debs/
+
+# pour restarter apache2
 sudo chmod 755 /var/www/html/debs
-sudo a2enmod autoindex
+sudo a2enmod cgi
 sudo systemctl restart apache2
 
 
